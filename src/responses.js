f9714c3b9033b1de9fce385a77ff7beda2207b90
IgorPolotai <igor.polotai@gmail.com>
2024-10-09 18:23:41 -0400

    Got eslint working, fixed eslint errors.

--- src/responses.js
+++ src/responses.js
@@ -4,215 +4,230 @@ const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 const css = fs.readFileSync(`${__dirname}/../client/style.css`);
 const data = JSON.parse(fs.readFileSync(`${__dirname}/../data/countries.json`));
 
-//Gets the index html
+// Gets the index html
 const getIndex = (request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.write(index);
-    response.end();
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(index);
+  response.end();
 };
 
-//Gets the CSS file
+// Gets the CSS file
 const getCSS = (request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/css' });
-    response.write(css);
-    response.end();
+  response.writeHead(200, { 'Content-Type': 'text/css' });
+  response.write(css);
+  response.end();
 };
 
-//Sends back a JSON response
+// Sends back a JSON response
 const respondJSON = (request, response, status, object) => {
-    const content = JSON.stringify(object);
-    console.log(content);
-    response.writeHead(status, {
-        'Content-Type': 'application/json',
-        'Content-Length': Buffer.byteLength(content, 'utf8')
-    });
+  const content = JSON.stringify(object);
+  console.log(content);
+  response.writeHead(status, {
+    'Content-Type': 'application/json',
+    'Content-Length': Buffer.byteLength(content, 'utf8'),
+  });
 
-    if (request.method !== 'HEAD' && status !== 204) {
-        response.write(content);
-    }
+  if (request.method !== 'HEAD' && status !== 204) {
+    response.write(content);
+  }
 
-    response.end();
+  response.end();
 };
 
 const getCountry = (request, response) => {
-    const name = (request.query.name).toLowerCase();
-    const filtered = data.find((country) => country.name.toLowerCase() === name);
-    
-    if (filtered) { respondJSON(request, response, 200, filtered); } 
-    else { respondJSON(request, response, 404, { message: "Country not found" }); }
+  const name = (request.query.name).toLowerCase();
+  const filtered = data.find((country) => country.name.toLowerCase() === name);
+
+  if (filtered) { respondJSON(request, response, 200, filtered); } else { respondJSON(request, response, 404, { message: 'Country not found' }); }
 };
 
-//Returns all of the countries within the latitude and longitude ranges
+// Returns all of the countries within the latitude and longitude ranges
 const getCountries = (request, response) => {
-    // Check for the existence of required parameters
-    if (!request.query.latmin || !request.query.latmax || 
-        !request.query.longmin || !request.query.longmax) {
-        return respondJSON(request, response, 404, { message: "All four parameters are required.", id: "missingParams" });
-    }
+  // Check for the existence of required parameters
+  if (!request.query.latmin || !request.query.latmax
+        || !request.query.longmin || !request.query.longmax) {
+    return respondJSON(request, response, 404, { message: 'All four parameters are required.', id: 'missingParams' });
+  }
 
-    // Convert query parameters to numbers
-    const latmin = parseFloat(request.query.latmin);
-    const latmax = parseFloat(request.query.latmax);
-    const longmin = parseFloat(request.query.longmin);
-    const longmax = parseFloat(request.query.longmax);
+  // Convert query parameters to numbers
+  const latmin = parseFloat(request.query.latmin);
+  const latmax = parseFloat(request.query.latmax);
+  const longmin = parseFloat(request.query.longmin);
+  const longmax = parseFloat(request.query.longmax);
 
-    // Check if the converted parameters are valid numbers
-    if (isNaN(latmin) || isNaN(latmax) || isNaN(longmin) || isNaN(longmax)) {
-        return respondJSON(request, response, 404, { message: "One or more of the parameters were not numbers.", id: "invalidParams" });
-    }
+  // Check if the converted parameters are valid numbers
+  if (Number.isNaN(latmin) || Number.isNaN(latmax)
+      || Number.isNaN(longmin) || Number.isNaN(longmax)) {
+    return respondJSON(request, response, 404, { message: 'One or more of the parameters were not numbers.', id: 'invalidParams' });
+  }
 
-    // Filter countries based on latitude and longitude ranges
-    const filtered = data.filter((country) => 
-        parseFloat(country.latitude) >= latmin && 
-        parseFloat(country.latitude) <= latmax &&
-        parseFloat(country.longitude) >= longmin &&
-        parseFloat(country.longitude) <= longmax
-    );
+  // Filter countries based on latitude and longitude ranges
+  const filtered = data.filter((country) => parseFloat(country.latitude) >= latmin
+        && parseFloat(country.latitude) <= latmax
+        && parseFloat(country.longitude) >= longmin
+        && parseFloat(country.longitude) <= longmax);
 
-    // Return filtered results
-    if (filtered.length > 0) {
-        return respondJSON(request, response, 200, filtered); 
-    } else {
-        return respondJSON(request, response, 404, { message: "No countries found within the specified range." });
-    }
+  // Return filtered results
+  if (filtered.length > 0) {
+    return respondJSON(request, response, 200, filtered);
+  }
+  return respondJSON(request, response, 404, { message: 'No countries found within the specified range.' });
 };
-
 
 // return JSON of all countries data
 const getAllCountries = (request, response) => {
-    const responseJSON = {
-        data,
-    };
+  const responseJSON = {
+    data,
+  };
 
-    respondJSON(request, response, 200, responseJSON);
+  respondJSON(request, response, 200, responseJSON);
 };
 
 const getRegion = (request, response) => {
-    if ((!request.query.subregion && !request.query.region)
+  if ((!request.query.subregion && !request.query.region)
         || (request.query.subregion && request.query.region)) {
-        return respondJSON(request, response, 404, { message: "Either a region or a subregion is allowed. Neither or both is forbidden.", id: "invalidParams" });
-    }
+    return respondJSON(request, response, 404, { message: 'Either a region or a subregion is allowed. Neither or both is forbidden.', id: 'invalidParams' });
+  }
 
-    let filtered = {};
+  let filtered = {};
 
-    if(request.query.region) {
-        const region = (request.query.region).toLowerCase();
-        filtered = data.filter((country) => country.region.toLowerCase() === region);
-    }
-    else if (request.query.subregion) {
-        const subregion = (request.query.subregion).toLowerCase();
-        filtered = data.filter((country) => country.subregion.toLowerCase() === subregion);
-    }
-    
-    if (filtered) { return respondJSON(request, response, 200, filtered); } 
-    else { return respondJSON(request, response, 404, { message: "Country not found" }); }
+  if (request.query.region) {
+    const region = (request.query.region).toLowerCase();
+    filtered = data.filter((country) => country.region.toLowerCase() === region);
+  } else if (request.query.subregion) {
+    const subregion = (request.query.subregion).toLowerCase();
+    filtered = data.filter((country) => country.subregion.toLowerCase() === subregion);
+  }
+
+  if (filtered) { return respondJSON(request, response, 200, filtered); }
+  return respondJSON(request, response, 404, { message: 'Country not found' });
 };
 
-//http://127.0.0.1:3000/addCountry?name=e&capital=e&currency=e&
-//currency_name=w&currency_symbol=3&region=3&subregion=23&nationality=djs&zoneName=3&
-//gmtOffset=sss&gmtOffsetName=sss&abbreviation=e&tzName=sjsjs&latitude=1.000&longitude=1.999
+// http://127.0.0.1:3000/addCountry?name=e&capital=e&currency=e&
+// currency_name=w&currency_symbol=3&region=3&subregion=23&nationality=djs&zoneName=3&
+// gmtOffset=sss&gmtOffsetName=sss&abbreviation=e&tzName=sjsjs&latitude=1.000&longitude=1.999
 
 // uses a POST to update or add a country
 const addCountry = (request, response) => {
-    // default json message
-    const responseJSON = {
-        message: 'All parameters are required.',
-    };
+  // default json message
+  const responseJSON = {
+    message: 'All parameters are required.',
+  };
 
-    const { name, 
-            capital, 
-            currency, 
-            currency_name, 
-            currency_symbol,
-            region,
-            subregion,
-            nationality,
-            zoneName,
-            gmtOffset,
-            gmtOffsetName,
-            abbreviation,
-            tzName,
-            latitude,
-            longitude } = request.body;
+  const {
+    name,
+    capital,
+    currency,
+    currencyName,
+    currencySymbol,
+    region,
+    subregion,
+    nationality,
+    zoneName,
+    gmtOffset,
+    gmtOffsetName,
+    abbreviation,
+    tzName,
+    latitude,
+    longitude,
+  } = request.body;
 
-    if (!name || !capital || !currency ||
-        !currency_name || !currency_symbol ||
-        !region || !subregion || 
-        !nationality || !zoneName ||
-        !gmtOffset || !gmtOffsetName || !abbreviation
+  console.log("Name: " + name);
+  console.log("Capital: " + capital);
+  console.log("Currency: " + currency);
+  console.log("Currency Symbol:" + currencySymbol);
+  console.log("Region: " + region);
+  console.log("Subregion: " + subregion);
+  console.log("Nationality: " + nationality);
+  console.log("Zone Name: " + zoneName);
+  console.log("GMT Offset: " + gmtOffset);
+  console.log("GMT Offset Name: " + gmtOffsetName);
+  console.log("Abbreviation: " + abbreviation);
+  console.log("TZ Name: " + tzName);
+  console.log("Latitude: " + latitude);
+  console.log("Longitude: " + longitude);
+
+
+  if (!name || !capital || !currency
+        || !currencyName || !currencySymbol
+        || !region || !subregion
+        || !nationality || !zoneName
+        || !gmtOffset || !gmtOffsetName || !abbreviation
         || !tzName || !latitude || !longitude
-    ) {
-        responseJSON.id = 'missingParams';
-        return respondJSON(request, response, 400, responseJSON);
-    }
+  ) {
+    responseJSON.id = 'missingParams';
+    return respondJSON(request, response, 400, responseJSON);
+  }
 
-    let responseCode = 204;
+  let responseCode = 204;
 
-    // If the country doesn't exist yet
-    if (!data[name]) {
-        responseCode = 201;
-        data[name] = {
-            name,
-            capital,
-            finance: {
-                currency,
-                currency_name,
-                currency_symbol
-            },
-            region,
-            subregion,
-            nationality,
-            timezones: {
-                zoneName,
-                gmtOffset,
-                gmtOffsetName,
-                abbreviation,
-                tzName
-            },
-            latitude,
-            longitude,
-        }
-    }
+  // If the country doesn't exist yet
+  if (!data[name]) {
+    responseCode = 201;
+    data[name] = {
+      name,
+      capital,
+      finance: {
+        currency,
+        currency_name: currencyName,
+        currency_symbol: currencySymbol,
+      },
+      region,
+      subregion,
+      nationality,
+      timezones: {
+        zoneName,
+        gmtOffset,
+        gmtOffsetName,
+        abbreviation,
+        tzName,
+      },
+      latitude,
+      longitude,
+    };
+  }
 
-    data[name].capital = capital;
-    data[name].finance.currency = currency;
-    data[name].finance.currency_name = currency_name;
-    data[name].finance.currency_symbol = currency_symbol;
-    data[name].region = region;
-    data[name].subregion = subregion;
-    data[name].nationality = nationality;
-    data[name].timezones.zoneName = zoneName;
-    data[name].timezones.gmtOffset = gmtOffset;
-    data[name].timezones.gmtOffsetName = gmtOffsetName;
-    data[name].timezones.abbreviation = abbreviation;
-    data[name].timezones.tzName = tzName;
-    data[name].latitude = latitude;
-    data[name].longitude = longitude;
+  data[name].capital = capital;
+  data[name].finance.currency = currency;
+  data[name].finance.currency_name = currencyName;
+  data[name].finance.currency_symbol = currencySymbol;
+  data[name].region = region;
+  data[name].subregion = subregion;
+  data[name].nationality = nationality;
+  data[name].timezones.zoneName = zoneName;
+  data[name].timezones.gmtOffset = gmtOffset;
+  data[name].timezones.gmtOffsetName = gmtOffsetName;
+  data[name].timezones.abbreviation = abbreviation;
+  data[name].timezones.tzName = tzName;
+  data[name].latitude = latitude;
+  data[name].longitude = longitude;
 
-    if (responseCode === 201) {
-        responseJSON.message = 'Created Successfully';
-        return respondJSON(request, response, responseCode, responseJSON);
-    }
+  if (responseCode === 201) {
+    responseJSON.message = 'Created Successfully';
+    return respondJSON(request, response, responseCode, responseJSON);
+  }
 
-    return respondJSON(request, response, responseCode, {});
+  return respondJSON(request, response, responseCode, {});
 };
 
-const addReview = (request, response) => {
+const addReview = (request, response) => respondJSON(request, response, 404, {});
 
-};
-
-//Gets a not found error response
-const notFound = (request, response) => respondJSON(request, response, 404,
-    { message: "The page you are looking for was not found", id: "notFound" });
+// Gets a not found error response
+const notFound = (request, response) => respondJSON(
+  request,
+  response,
+  404,
+  { message: 'The page you are looking for was not found', id: 'notFound' },
+);
 
 module.exports = {
-    getIndex,
-    getCSS,
-    getCountry,
-    getCountries,
-    getAllCountries,
-    getRegion,
-    addCountry,
-    addReview,
-    notFound
+  getIndex,
+  getCSS,
+  getCountry,
+  getCountries,
+  getAllCountries,
+  getRegion,
+  addCountry,
+  addReview,
+  notFound,
 };
--- src/server.js
+++ src/server.js
@@ -6,88 +6,79 @@ const responseHandler = require('./responses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
-    '/': responseHandler.getIndex,
-    '/style.css': responseHandler.getCSS,
-    '/getCountry': responseHandler.getCountry, //Get #1 (search by country name)
-    '/getCountries': responseHandler.getCountries, //Get #2, filtered by lat and lon ranges
-    '/getAllCountries': responseHandler.getAllCountries, //Get #3. Returns the whole JSON
-    '/getRegion': responseHandler.getCountry, //Get #4 (search by region, return all countries from there)
-    '/addCountry': responseHandler.addCountry, //Post #1 
-    '/addReview': responseHandler.addReview, //Post #2
-    notFound: responseHandler.notFound
-}
+  '/': responseHandler.getIndex,
+  '/style.css': responseHandler.getCSS,
+  '/getCountry': responseHandler.getCountry, // Get #1 (search by country name)
+  '/getCountries': responseHandler.getCountries, // Get #2, filtered by lat and lon ranges
+  '/getAllCountries': responseHandler.getAllCountries, // Get #3. Returns the whole JSON
+  '/getRegion': responseHandler.getCountry, // Get #4 (search by region, return all countries from there)
+  '/addCountry': responseHandler.addCountry, // Post #1
+  '/addReview': responseHandler.addReview, // Post #2
+  notFound: responseHandler.notFound,
+};
 
 // Takes all of the packets of data and builds out the body
 const parseBody = (request, response, handler) => {
-    const body = [];
+  const body = [];
 
-    request.on('error', (err) => {
-        console.dir(err);
+  request.on('error', (err) => {
+    console.dir(err);
+    response.statusCode = 400;
+    response.end();
+  });
+
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
+
+  request.on('end', () => {
+    const bodyString = Buffer.concat(body).toString();
+
+    // Determine content type and parse accordingly
+    if (request.headers['Content-Type'] === 'application/json') {
+      try {
+        request.body = JSON.parse(bodyString);
+      } catch (err) {
         response.statusCode = 400;
         response.end();
-    });
+        return;
+      }
+    } else {
+      request.body = query.parse(bodyString);
+    }
 
-    request.on('data', (chunk) => {
-        body.push(chunk);
-    });
-
-    request.on('end', () => {
-        const bodyString = Buffer.concat(body).toString();
-        
-        // Determine content type and parse accordingly
-        if (request.headers['Content-Type'] === 'application/json') {
-            try {
-                request.body = JSON.parse(bodyString);
-            } catch (err) {
-                response.statusCode = 400;
-                response.end();
-                return;
-            }
-        } else {
-            request.body = query.parse(bodyString);
-        }
-        
-        handler(request, response);
-    });
+    handler(request, response);
+  });
 };
-
 
 // handle POST requests
 const handlePost = (request, response, parsedUrl) => {
-    if (parsedUrl.pathname === '/addCountry') { parseBody(request, response, responseHandler.addCountry); }
-    else if (parsedUrl.pathname === '/addReview') { parseBody(request, response, responseHandler.addReview); }
-    else { responseHandler.notFound(request, response); }
+  if (parsedUrl.pathname === '/addCountry') { parseBody(request, response, responseHandler.addCountry); } else if (parsedUrl.pathname === '/addReview') { parseBody(request, response, responseHandler.addReview); } else { responseHandler.notFound(request, response); }
 };
 
 // handle GET requests
 const handleGet = (request, response, parsedUrl) => {
-    if (parsedUrl.pathname === '/') { responseHandler.getIndex(request, response); }
-    else if (parsedUrl.pathname === '/style.css') { responseHandler.getCSS(request, response); } 
-    else if (parsedUrl.pathname === '/getCountry') { responseHandler.getCountry(request, response); }
-    else if (parsedUrl.pathname === '/getCountries') { responseHandler.getCountries(request, response); } 
-    else if (parsedUrl.pathname === '/getAllCountries') { responseHandler.getAllCountries(request, response); }
-    else if (parsedUrl.pathname === '/getRegion') { responseHandler.getRegion(request, response); }
-    else { responseHandler.notFound(request, response); }
+  if (parsedUrl.pathname === '/') { responseHandler.getIndex(request, response); } else if (parsedUrl.pathname === '/style.css') { responseHandler.getCSS(request, response); } else if (parsedUrl.pathname === '/getCountry') { responseHandler.getCountry(request, response); } else if (parsedUrl.pathname === '/getCountries') { responseHandler.getCountries(request, response); } else if (parsedUrl.pathname === '/getAllCountries') { responseHandler.getAllCountries(request, response); } else if (parsedUrl.pathname === '/getRegion') { responseHandler.getRegion(request, response); } else { responseHandler.notFound(request, response); }
 };
 
 // routes our requests to the correct endpoint
 const onRequest = (request, response) => {
-    const protocol = request.connection.encrypted ? 'https' : 'http';
-    const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
+  const protocol = request.connection.encrypted ? 'https' : 'http';
+  const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
 
-    request.query = Object.fromEntries(parsedUrl.searchParams);
+  request.query = Object.fromEntries(parsedUrl.searchParams);
 
-    if (urlStruct[parsedUrl.pathname]) {
-        if (request.method === 'POST') {
-            handlePost(request, response, parsedUrl);
-        } else {
-            handleGet(request, response, parsedUrl);
-        }
-    } else { // If the page does not exist
-        urlStruct.notFound(request, response);
+  if (urlStruct[parsedUrl.pathname]) {
+    if (request.method === 'POST') {
+      handlePost(request, response, parsedUrl);
+    } else {
+      handleGet(request, response, parsedUrl);
     }
-}; 
+  } else { // If the page does not exist
+    urlStruct.notFound(request, response);
+  }
+};
 
 http.createServer(onRequest).listen(port, () => {
-    console.log(`Listening on 127.0.0.1: ${port}`);
+  console.log(`Listening on 127.0.0.1: ${port}`);
 });
